5db9d58e51ee2663072efb6d70edd91645cc8b8b
Alexander Suarez <alexander.suarez@students.plymouth.ac.uk>
2023-08-19 18:03:53 +0100

    ssh encryption

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@ SERVER_IP="103.241.64.253"
 SERVER_USER="root"
 APP_DIR="/var/www/face-reality/"
 
-# Decrypt the private key
-openssl aes-256-cbc -K $encrypted_db2095f63ba3_key -iv $encrypted_db2095f63ba3_iv -in deploy_rsa.enc -out deploy_rsa -d
-
 # Set the required permissions for the private key
 chmod 600 deploy_rsa
 
@@ -16,5 +13,3 @@ ssh-add deploy_rsa
 
 # Transfer files to server
 rsync -avz --delete dist/frontend/ $SERVER_USER@$SERVER_IP:$APP_DIR
-
-
